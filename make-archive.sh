394215183858d872002f7405965b88438e6dc3ed
Richard Darst <rkd@zgib.net>
2026-02-16 22:22:57 +0200

    firefox ext name

--- make-archive.sh
+++ make-archive.sh
@@ -1,7 +1,14 @@
 # Create the zipfile that is the extension.
 # This does not yet work, since the .xpi file would have to be signed.
+
+cd $(dirname $0)
+mkdir -p build
+
+echo '<a href="firefox-ext-delete-outlook-cookies.xpi">firefox-ext-delete-outlook-cookies.xpi</a>' > build/index.html
+
 cd extension
-zip ../browser-ext-delete-outlook-cookies.xpi \
+# Firefox version
+zip ../build/firefox-ext-delete-outlook-cookies.xpi \
     manifest.json \
     background.js \
     icon.ico
