0ea325f376fcb22f5ac484df3e75c90e8a6bc36f
Richard Darst <rkd@zgib.net>
2026-02-16 21:46:26 +0200

    add workflow

--- make-archive.sh
+++ make-archive.sh
@@ -1,7 +1,7 @@
 # Create the zipfile that is the extension.
 # This does not yet work, since the .xpi file would have to be signed.
 cd extension
-zip ../firefox-ext-delete-outlook-cookies.xpi \
+zip ../browser-ext-delete-outlook-cookies.xpi \
     manifest.json \
     background.js \
     icon.ico
