a8ead1d9ed3cd18656922ab0183d6f3481624910
Richard Darst <rkd@zgib.net>
2026-02-17 21:39:29 +0200

    update build

--- make-archive.sh
+++ make-archive.sh
@@ -1,16 +1,24 @@
 # Create the zipfile that is the extension.
 # This does not yet work, since the .xpi file would have to be signed.
 
+#set -e
+
 cd $(dirname $0)
 mkdir -p build
 
-echo '<a href="firefox-ext-delete-outlook-cookies.xpi">firefox-ext-delete-outlook-cookies.xpi</a>' > build/index.html
-
 cp firefox-update-manifest.json build/
+cp release/*.xpi build/
 
-cd extension
+pushd extension/   # cd and save dir
 # Firefox version
 zip ../build/firefox-ext-delete-outlook-cookies.xpi \
     manifest.json \
     background.js \
     icon.ico
+popd               # change back to old directory
+
+
+rm -f build/index.html
+for file in $( ls build/*.xpi ); do
+    echo "<a href=\"$file\">$file</a><br>" >> build/index.html
+done
