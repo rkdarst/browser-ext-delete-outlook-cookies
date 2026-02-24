168e6a7daa5f1b088bb519878c58a52622edc450
Richard Darst <rkd@zgib.net>
2026-02-24 22:05:07 +0200

    extension/background: improve debug logging

--- extension/background.js
+++ extension/background.js
@@ -6,17 +6,18 @@ async function deleteOfficeCookies() {
     // in a container other than default.
     const stores = await browser.cookies.getAllCookieStores();
     for (let store of stores) {
-      console.log("  Cookie store: ", store);
+      console.log(`  Cookie store: ${store.id}`);
+      console.debug(" ", store)
       //console.log("store:", store.id)
       // This function will only return cookies for which we have host
       // permission.  Anyway, we limit it to the target domain here.
       for (let targetDomain of targetDomains) {
         console.log(  "    targetDomain: ", targetDomain);
         let cookies = await browser.cookies.getAll({ domain: targetDomain, storeId: store.id, firstPartyDomain: null });
-        console.log("    ", cookies);
+        console.debug("    ", cookies);
         for (let cookie of cookies) {
           // Construct the URL from cookie info
-          console.log("      Cookie: ", cookie);
+          console.info("      Cookie: ", cookie);
           let url =
             (cookie.secure ? "https://" : "http://") +
             cookie.domain.replace(/^\./, "") +
@@ -27,7 +28,7 @@ async function deleteOfficeCookies() {
             name: cookie.name,
             storeId: cookie.storeId
           });
-          console.log(`      Deleted cookie: ${cookie.name}`);
+          console.log(`      Deleted cookie: pk=${cookie.partitionKey} d=${cookie.domain} n=${cookie.name} p=${cookie.path} fpd=${cookie.firstPartyDomain}`);
         }
       }
     }
